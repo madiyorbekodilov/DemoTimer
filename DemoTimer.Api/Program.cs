bb14e1871bd89a8c229ce3cbdddc1c42f56e465a
Ferganskiy <odilovmadiyor029@gmail.com>
2023-12-03 20:54:28 +0500

    bugs fixed

--- DemoTimer.Api/Program.cs
+++ DemoTimer.Api/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddSwaggerGen();
 
 // Add AbbDbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
