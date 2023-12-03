6cfb17195522123ab56631752b01d003d1eee921
Ferganskiy <odilovmadiyor029@gmail.com>
2023-12-03 17:51:29 +0500

    AppDbContext added

---
+++ DemoTimer.Api/Data/Contexts/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DemoTimer.Api.Data.Contexts;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {   }
+}
