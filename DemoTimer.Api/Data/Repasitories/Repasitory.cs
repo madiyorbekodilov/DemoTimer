1c531e9a3e636a918834a22be346ef530758b3b0
Ferganskiy <odilovmadiyor029@gmail.com>
2023-12-03 18:02:54 +0500

    Repasitories added

---
+++ DemoTimer.Api/Data/Repasitories/Repasitory.cs
@@ -0,0 +1,37 @@
+﻿using DemoTimer.Api.Data.Contexts;
+using DemoTimer.Api.Data.IRepasitories;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoTimer.Api.Data.Repasitories;
+
+public class Repasitory<T> : IRepasitory<T> where T : class
+{
+    private readonly AppDbContext appDbContext;
+    private readonly DbSet<T> dbSet;
+    public Repasitory(AppDbContext appDbContext)
+    {
+        this.appDbContext = appDbContext;
+        dbSet = this.appDbContext.Set<T>();
+    }
+    public async Task<T> CreateAsync(T entity)
+    {
+        await this.dbSet.AddAsync(entity);
+        return entity;
+    }
+
+    public void Destroy(T entity)
+    {
+        this.dbSet.Remove(entity);
+    }
+
+    public async Task SaveAsync()
+    {
+        await appDbContext.SaveChangesAsync();
+    }
+
+    public IQueryable<T> SelectAll()
+    {
+        IQueryable<T> query = dbSet.AsQueryable();
+        return query;
+    }
+}
